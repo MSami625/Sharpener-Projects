362e43784c2a124a3097a22dc1a3e8d03a822a9e
M Sami <beboolean@gmail.com>
2024-08-14 23:35:35 +0530

    Basic Server using Node js Http module

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World"); 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("sami");
+    res.end("Hello World");
+});
+
+server.listen(4000);
