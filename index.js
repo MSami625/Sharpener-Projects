5519a9795af85a486dfdb1b2c86b0d5f2e4f7bd7
M Sami <beboolean@gmail.com>
2024-08-15 19:33:41 +0530

    Using req.url to send response on different pages

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("sami");
-    res.end("Hello World");
+ 
+  if (req.url === "/home") {
+  
+    res.write("<html>");
+    res.write("<head><title>Home</title></head>");
+    res.write("<body><h1>Welcome to Home Page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (req.url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>About</title></head>");
+    res.write("<body><h1>Welcome to About Page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (req.url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>Node</title></head>");
+    res.write("<body><h1>Welcome to Node Page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
 });
 
 server.listen(4000);
