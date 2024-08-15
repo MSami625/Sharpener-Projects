473f8847c87cb0421ed7f0ba9fccc0a763aa8cd5
M Sami <beboolean@gmail.com>
2024-08-15 21:39:45 +0530

    Clean up and structure files

--- SimpleMsgFrom.js
+++ SimpleMsgFrom.js
@@ -1,48 +1,9 @@
 const http=require('http');
-const fs=require('fs');
 
-const server = http.createServer((req,res)=>{
-
-    if(req.url==="/"){
-
-       fs.readFile("message.txt","utf8",(err,data)=>{ 
-             if(err){
-                 console.log(err)
-                 return res.end();
-             }
-
-          
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title/><head>')
-        res.write(`<h1>${data}</h1>`)
-        res.write('<body><form action="/message" method="POST"  ><input type="text" name="message" /><button>Send</button></form></body>')
-        res.write('</html>')
-        return res.end()
-    })
-    }
+const routes=require('./routes');
 
 
-
-
-    if(req.url==="/message" && req.method==="POST"){
-
-        const body=[];
-        req.on("data",(chunk)=>{
-           body.push(chunk);
-        })
-
-        req.on("end",()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            const message=parsedBody.split("=")[1];
-            fs.writeFileSync('message.txt',message);
-        })
-        
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end()
-    }
-})
+const server = http.createServer(routes)
 
 
 
