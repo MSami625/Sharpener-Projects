84bd20a34450f13baf8b24a58805be70b828596b
M Sami <beboolean@gmail.com>
2024-08-16 19:23:58 +0530

    Using Body Parser

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app=express();
 
-app.use((req,res,next)=>{
-  console.log('First Middleware');
-  next();
-});
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/add-product',(req,res,next)=>{
+  res.send("<form action='/product' method='POST' >Name:<input type='text' name='product'>Size: <input type='number' name='size'/><button type='submit'>Add Product</button></form>");
+})
+
+app.post('/product',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');      
+})
+
 
 app.use((req,res,next)=>{
-  console.log("Second Middleware")
   res.send("<h1>Hello from Express</h1>") 
 })
 
