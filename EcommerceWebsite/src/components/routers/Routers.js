a91092e93d2ee2bf1b36149df258a3a6eded0b79
M Sami <beboolean@gmail.com>
2024-11-07 01:10:50 +0530

    React Router added

--- EcommerceWebsite/src/components/routers/Routers.js
+++ EcommerceWebsite/src/components/routers/Routers.js
@@ -1,12 +1,14 @@
 import ProductList from "../products/ProductList";
 import About from "../layout/About";
 import Home from "../layout/Home";
+import ContactUs from "../layout/ContactUs";
 import { Route, Routes } from "react-router-dom";
 
 const routePath = {
   Home: "/",
   Store: "/store",
   About: "/about",
+  ContactUs: "/contact-us",
 };
 
 const Routers = () => {
@@ -16,6 +18,7 @@ const Routers = () => {
         <Route path={routePath.Home} element={<Home />} />
         <Route path={routePath.Store} element={<ProductList />} />
         <Route path={routePath.About} element={<About />} />
+        <Route path={routePath.ContactUs} element={<ContactUs />} />
       </Routes>
     </div>
   );
