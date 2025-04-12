23be8896c09482a4ffa75ea54ce64d68d81947f6
M Sami <beboolean@gmail.com>
2025-04-12 22:57:47 +0530

    Feat: Add Expenses

--- ExpenseTrackerReact/src/Store/AuthContext.js
+++ ExpenseTrackerReact/src/Store/AuthContext.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   token: "",
+  email: "",
   isLoggedIn: false,
   login: (token) => {},
   logout: () => {},
@@ -9,18 +10,24 @@ const AuthContext = React.createContext({
 
 export const AuthContextProvider = (props) => {
   const initialState = localStorage.getItem("token");
+  const initialEmail = localStorage.getItem("email");
   const [token, setToken] = useState(initialState);
+  const [email, setEmail] = useState(initialEmail);
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => {
+  const loginHandler = (token, email) => {
     setToken(token);
+    setEmail(email);
     localStorage.setItem("token", token);
+    localStorage.setItem("email", email);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    setEmail(null);
     localStorage.removeItem("token");
+    localStorage.removeItem("email");
   };
 
   useEffect(() => {
@@ -30,13 +37,14 @@ export const AuthContextProvider = (props) => {
       logoutTimer = setTimeout(() => {
         logoutHandler();
         alert("You have been logged out due to inactivity.");
-      }, 5 * 60 * 1000);
+      }, 60 * 60 * 1000);
     }
     return () => clearTimeout(logoutTimer);
   }, [userIsLoggedIn]);
 
   const contextValue = {
     token: token,
+    email: email,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
     logout: logoutHandler,
